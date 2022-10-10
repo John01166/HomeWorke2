a5fa0b7fa1d406e825b7a342aee0ccc1202a22fd
John <John01166@ya.ru>
2022-10-10 11:41:39 +0300

    DZ10

--- DZ10/Program.cs
+++ DZ10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на 
+вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ */
+
+System.Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine($"Вторая цифра этого числа: {num /10 %10}");
